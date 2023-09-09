5d2019a2fc13a35ed3911fbc3c59aa479c265899
Elena <senusya@gmail.com>
2023-09-09 14:44:07 +0300

    diploma

--- main.py
+++
@@ -1,9 +0,0 @@
-def in_autotests_we_trust(a, b):
-    if a == b:
-        print('Тест пройден')
-    else:
-        print('Тест провален')
-
-in_autotests_we_trust(10, '10')
-
-in_autotests_we_trust(0, False)
